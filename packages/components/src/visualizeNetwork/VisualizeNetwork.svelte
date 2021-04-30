e2909e6f32a82bbd5bb8062dd69f856f363b3545
Max Richter <max.richter@smail.th-koeln.de>
2021-04-30 13:17:10 +0200

    merge main

--- packages/components/src/visualizeNetwork/VisualizeNetwork.svelte
+++ packages/components/src/visualizeNetwork/VisualizeNetwork.svelte
@@ -1,90 +1,98 @@
 <script lang="ts">
-    import type { Sequential } from "@tensorflow/tfjs";
+  import type { Sequential } from "@tensorflow/tfjs";
 
-    export let neuralNet: Sequential;
+  export let neuralNet: Sequential;
 
-    function getNeurons(nn): { x: number; y: number }[][] {
-        let nodes = [];
-        nodes.push(nn.layers[0].input.shape[1]);
-        for (let i = 0; i < nn.layers.length; i++) {
-            nodes.push(nn.layers[i].units);
-        }
-
-        const _neurons = [];
-
-        for (let i = 0; i < nodes.length; i++) {
-            let temp = [];
-            for (let j = 0; j < nodes[i]; j++) {
-                let x = (i / (nodes.length - 1)) * 100;
-                let y = (j / nodes[i]) * 100;
-                const pos = { x, y };
-                temp.push(pos);
-            }
-
-            _neurons.push(temp);
-        }
-
-        return _neurons;
+  function getNeurons(nn): { x: number; y: number }[][] {
+    let nodes = [];
+    nodes.push(nn.layers[0].input.shape[1]);
+    for (let i = 0; i < nn.layers.length; i++) {
+      nodes.push(nn.layers[i].units);
     }
 
-    function getWeights(nn: Sequential) {
-        const weights = [];
-        const biases = [];
+    const _neurons = [];
 
-        for (let i = 0; i < nn.layers.length; i++) {
-            let a = nn.layers[i].getWeights()[0];
-            let b = a.arraySync();
-            weights.push(b);
+    for (let i = 0; i < nodes.length; i++) {
+      let temp = [];
+      for (let j = 0; j < nodes[i]; j++) {
+        let x = (i / (nodes.length - 1)) * 100;
+        let y = (j / nodes[i]) * 100;
+        const pos = { x, y };
+        temp.push(pos);
+      }
 
-            let c = nn.layers[i].getWeights()[1];
-            let d = c.arraySync();
-            biases.push(d);
-        }
-
-        return weights;
+      _neurons.push(temp);
     }
 
-    $: layers = neuralNet && getNeurons(neuralNet);
-    $: weights = neuralNet && getWeights(neuralNet);
+    return _neurons;
+  }
+
+  function getWeights(nn: Sequential) {
+    const weights = [];
+    const biases = [];
+
+    for (let i = 0; i < nn.layers.length; i++) {
+      let a = nn.layers[i].getWeights()[0];
+      let b = a.arraySync();
+      weights.push(b);
+
+      let c = nn.layers[i].getWeights()[1];
+      let d = c.arraySync();
+      biases.push(d);
+    }
+
+    return weights;
+  }
+
+  $: layers = neuralNet && getNeurons(neuralNet);
+  $: weights = neuralNet && getWeights(neuralNet);
+
+  let scale = 0;
+  let px = 0;
+  let py = 0;
 </script>
 
-<svg width="640" height="480" viewBox="0 0 100 100">
-
+<div class="wrapper">
+  <svg
+    width="640"
+    height="480"
+    viewBox="0 0 100 100"
+    style={`transform: scale(${scale}), translate(${px},${py})`}
+  >
     {#each weights as layer, i}
-    {#each layer as neuron, j}
+      {#each layer as neuron, j}
         {#each neuron as singleWeight, k}
-            {#if layers[i+1][k]}
-                <line
-                    stroke-width={singleWeight*10}
-                    x1={layers[i][j].x}
-                    y1={layers[i][j].y}
-                    x2={layers[i + 1][k].x}
-                    y2={layers[i + 1][k].y}
-                />
-            {/if}
+          {#if layers[i + 1][k]}
+            <line
+              stroke-width={singleWeight * 10}
+              x1={layers[i][j].x}
+              y1={layers[i][j].y}
+              x2={layers[i + 1][k].x}
+              y2={layers[i + 1][k].y}
+            />
+          {/if}
         {/each}
+      {/each}
     {/each}
-{/each}
 
     {#each layers as layer}
-        <g>
-            {#each layer as n}
-                <circle cx={n.x} cy={n.y} r="0.5" />
-            {/each}
-        </g>
+      <g>
+        {#each layer as n}
+          <circle cx={n.x} cy={n.y} r="0.5" />
+        {/each}
+      </g>
     {/each}
-
-
-</svg>
+  </svg>
+</div>
 
 <style>
-    circle {
-        fill: red;
-    }
+  circle {
+    fill: red;
+  }
 
-    line {
-        stroke: white;
-        opacity: 0.5;
-        stroke-width: 0.1px;
-    }
+  line {
+    stroke: white;
+    opacity: 0.5;
+    stroke-width: 0.1px;
+  }
 </style>
